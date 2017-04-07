84620bcf1401809b35fd42ea22f7af005285a672
Ron <guangking1987@163.com>
2017-04-07 16:37:49 +0800

    update

--- functional_test.py
+++ functional_test.py
@@ -1,15 +1,30 @@
 # -*- coding: UTF-8 -*-
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import unittest,time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome("F:\soft\chromedriver.exe")
+        self.driver.implicitly_wait(3)
 
     def tearDown(self):
         self.driver.quit()
     def test_can_start_list_and_retrieve_it_later(self):
         self.driver.get('http://localhost:8000')
-        self.assertIn( 'To-Do', self.driver.title)
-        self.fail('Finish the test!')
+        self.assertIn( 'To-Do lists', self.driver.title)
+        header_text= self.driver.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        inputbox= self.driver.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.driver.find_element_by_id('id_list_table')
+        rows= table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+
+
+
 if __name__=='__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,32 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+        self.assertEqual(31,3)
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.strip().startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request= HttpRequest()
+        request.method='POST'
+        request.POST['item_text'] ='A new list item'
+        response=home_page(request)
+        self.assertIn('A new list item',response.content.decode())
+        excepted_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(),excepted_html)
